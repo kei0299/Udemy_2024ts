a23c2c071b5d0fd90ef0f0999accd84cf36e51df
kei0299 <kitagen_1026@yahoo.co.jp>
2025-01-11 21:02:55 +0900

    型定義

---
+++ ts_type/index.ts
@@ -0,0 +1,16 @@
+let hasValue: boolean = true;
+let count: number = 10;
+let float: number = 1.1;
+let negative: number = -10;
+let single: string = 'hello';
+let double: string = "hello";
+let back: string = `hello`;
+
+const person = {
+  name: {
+    first: 'Jack',
+    last: 'Smith'
+  },
+  age: 21
+}
+console.log(person.name)
